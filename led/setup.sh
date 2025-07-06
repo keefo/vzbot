7da68f6179b88fdf81dad7142feb9123965c6bef
Xu Lian <lianxu@me.com>
2025-07-05 18:00:18 -0700

    a

--- led/setup.sh
+++ led/setup.sh
@@ -2,7 +2,8 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-./remove.sh
+./remove.sh || true
+
 sudo cp led_service.service /etc/systemd/system/led_service.service
 sudo systemctl daemon-reexec
 sudo systemctl daemon-reload
